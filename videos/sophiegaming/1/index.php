d11d3cea2734adad1eeefe9f7071046229704604
urupeliss <155288055+urupeliss@users.noreply.github.com>
2025-02-12 08:50:00 -0300

    Add files via upload

---
+++ videos/sophiegaming/1/index.php
@@ -0,0 +1,34 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="../../../css/videos.css"/>
+</head>
+<body>
+        <div style="text-align:center;">
+			<div style="width:100%;background-color:#000;">
+            <object type="application/x-shockwave-flash" data="../../resources/player.swf" width="384" height="256">
+                <param name="wmode" value="transparent">
+                <param name="allowFullScreen" value="false">
+                <param name='flashvars' value='filename=sophiegaming-1-1.flv'>
+            </object>
+			</div>
+            PART 1 - <a href="index2.html">PART 2</a>
+            <h1>starting 2023 by playing TF2 on potato PCs</h1>
+            <div style="font-size:20px;"><?php class pageView { private $file = 'views.txt'; public function add_view() { $prev_count = file_get_contents($this->file); file_put_contents($this->file, $prev_count+1); } public function get_view() { $count = file_get_contents($this->file); return $count; } } $view = new pageView; $view->add_view(); echo $view->get_view().' views'; ?> - 
+			Jan 2, 2023
+			</div>
+        </div>
+        <table>
+            <tr>
+                <td> 
+                    <a href="../"><img src="../pfp.jpg" style="max-width:100px;"></a>
+                </td>
+                <td>
+                    <a href="../">sophieGaming</a>
+				</td>
+				<td style="padding-left:20px;">
+					<h2>Description</h2>
+					how else should we spend our first day of the new year than acting like it's 2012 with 2 old Windows 7 PCs and TF2 on mastercomfig's lowest offerings..?
+				</td>
+            </tr>
+            </table>
+    </body>
+</html>
